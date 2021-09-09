376ab71cabedb68815f9533dde5f7da351bc57d7
peax3 <peaceadjerese@gmail.com>
2021-09-09 12:45:19 +0100

    Add project files.

---
+++ Entities/Models/Issue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Models
+{
+	public class Issue
+	{
+		public Guid IssueId { get; set; }
+
+		[Required]
+		public string Subject { get; set; }
+
+		[Required]
+		public string Body { get; set; }
+
+		[Required]
+		public bool Pending { get; set; } = false;
+	}
+}
---
+++ Repository/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository
+{
+	public class AppDbContext : DbContext
+	{
+		public AppDbContext(DbContextOptions<AppDbContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Issue> Issues { get; set; }
+	}
+}
